0ae69c2dea4ff94cca05dcba6bbfe3eb4ef595cc
Benjamin Böcker <apps@ben-boecker.de>
2017-03-06 16:21:04 +0100

    Initial commit

---
+++ CachedDateFormatter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CachedDateFormatter
+//
+//  Created by Benni on 06.03.17.
+//  Copyright © 2017 Ben Böcker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
